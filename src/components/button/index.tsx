5a627c12d75bfca28462f9935d5f3a708f7de4bb
tangchenming <tangchenming@hikvision.com>
2024-04-19 17:40:02 +0800

    feat：button组件封装完成，可正常使用
    Signed-off-by: tangchenming <tangchenming@hikvision.com>

--- src/components/button/index.tsx
+++ src/components/button/index.tsx
@@ -1,16 +1,17 @@
 import React, { HTMLAttributes } from 'react'
 import './index.scss'
 
-type Size = Omit<'large' | 'small' | 'mini', 'mini' & 'small'>
-
 interface Props extends HTMLAttributes<HTMLButtonElement> {
   type?: `${ToyComponent.BaseTypes}`
-  size?: Size
+  size?: `${ToyComponent.Sizes}`
 }
 
 const Button: React.FC<Props> = (props) => {
   return (
-    <button className={`toy-button ${props.size} ${props.type}`}>
+    <button
+      className={`toy-button ${props.size} ${props.type}`}
+      {...props as HTMLAttributes<HTMLButtonElement>}
+    >
       { props.children }
     </button>
   )
@@ -18,7 +19,7 @@ const Button: React.FC<Props> = (props) => {
 
 Button.defaultProps = {
   type: 'default',
-  size: 'middle'
+  size: 'small'
 }
 
 export default Button
