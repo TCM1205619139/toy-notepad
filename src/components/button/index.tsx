1e839f47287e74380ccaa714d0fa70c660cec6ce
tangchenming <tangchenming@hikvision.com>
2024-04-26 17:41:54 +0800

    feat：Button组件兼容icon模式
    Signed-off-by: tangchenming <tangchenming@hikvision.com>

--- src/components/button/index.tsx
+++ src/components/button/index.tsx
@@ -1,16 +1,20 @@
-import React, { HTMLAttributes } from 'react'
+import React, { HTMLAttributes, ReactNode } from 'react'
 import './index.scss'
 
 type HTMLButtonAttributes = HTMLAttributes<HTMLButtonElement>
 
 type Props = HTMLButtonAttributes & {
   type?: `${ToyComponent.BaseTypes}`
-  size?: `${ToyComponent.Sizes}`
+  size?: `${ToyComponent.Sizes}`,
+  icon?: ReactNode
 }
 
-const Button: React.FC<Props> = ({ size, type, ...props }) => {
+const Button: React.FC<Props> = ({ size, type, icon, ...props }) => {
+  const isIconButton = !!icon
+
   return (
-    <button{ ...props } className={`toy-button ${size} ${type}`}>
+    <button{ ...props } className={`toy-button ${size} ${type} ${isIconButton ? 'icon-button' : ''}`}>
+      { icon }
       { props.children }
     </button>
   )
