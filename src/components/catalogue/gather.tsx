34162e0ffb6b2cbe3e94b2d03c79d4a42e9263f9
tangchenming <tangchenming@hikvision.com>
2024-05-21 16:13:07 +0800

    feat: 目录完成动画效果开发
    Signed-off-by: tangchenming <tangchenming@hikvision.com>

--- src/components/catalogue/gather.tsx
+++ src/components/catalogue/gather.tsx
@@ -1,5 +1,9 @@
-import React, { HTMLAttributes, useState } from 'react'
-import { CSSTransition } from 'react-transition-group'
+import React, {
+  HTMLAttributes,
+  useState,
+  useRef
+} from 'react'
+import { Transition } from 'react-transition-group'
 import { Icon } from '@/components'
 import Detail from '@/components/catalogue/detail'
 
@@ -11,8 +15,10 @@ type Props = HTMLDivElementAttributes & {
   gather: ToyComponent.CatalogueItem
 }
 
-const Gather: React.FC<Props> = ({ gather: propsGather, ...props }) => {
+const Gather: React.FC<Props> = ({gather: propsGather, ...props}) => {
   const [gather, setGather] = useState<ToyComponent.CatalogueItem>(propsGather)
+  const detail = useRef<HTMLDivElement>(null)
+
   const switchOpen = (item: ToyComponent.CatalogueItem) => {
     setGather({
       ...item,
@@ -20,32 +26,84 @@ const Gather: React.FC<Props> = ({ gather: propsGather, ...props }) => {
     })
   }
 
+  const onEnter = () => {
+    const el = detail.current as HTMLDivElement
+
+    el.dataset.maxHeight = el.clientHeight + 'px'
+    el.dataset.overflow = el.style.overflow
+    el.style.maxHeight = '0px'
+    el.style.overflow = 'hidden'
+    el.classList.add('collapse-transition')
+  }
+  const onEntering = () => {
+    const el = detail.current as HTMLDivElement
+
+    setTimeout(() => {
+      el.style.maxHeight = el.dataset.maxHeight || ''
+    })
+  }
+  const onEntered = () => {
+    const el = detail.current as HTMLDivElement
+
+    el.style.maxHeight = ''
+    el.style.overflow = ''
+    el.classList.remove('collapse-transition')
+  }
+  const onExit = () => {
+    const el = detail.current as HTMLDivElement
+
+    el.style.maxHeight = el.dataset.maxHeight || ''
+    el.style.overflow = 'hidden'
+    el.classList.add('collapse-transition')
+  }
+  const onExiting = () => {
+    const el = detail.current as HTMLDivElement
+
+    setTimeout(() => {
+      el.style.maxHeight = '0px'
+    })
+  }
+  const onExited = () => {
+    const el = detail.current as HTMLDivElement
+
+    el.style.maxHeight = '0px'
+    el.style.overflow = ''
+    el.classList.remove('collapse-transition')
+  }
+
   return (
-    <section { ...props } className="gather-container">
+    <section {...props} className="gather-container">
       <div
         className="title-container"
         onClick={() => switchOpen(gather)}
       >
         <Icon
-          name='arrow-down-filling'
+          name="arrow-down-filling"
           className={`arrow-filling ${gather.open ? 'down' : 'right'}`}
         />
-        <span className="title">{ gather.title }</span>
+        <span
+          className="title">{gather.title}</span>
       </div>
-      <CSSTransition
+      <Transition
         in={gather.open}
-        timeout={500}
-        className="spread"
+        nodeRef={detail}
+        timeout={100}
+        onEnter={onEnter}
+        onEntering={onEntering}
+        onEntered={onEntered}
+        onExit={onExit}
+        onExiting={onExiting}
+        onExited={onExited}
         unmountOnExit
       >
-        <div className="details">
+        <div className="details" ref={detail}>
           {
             (gather.children || []).map(detail => {
-              return <Detail key={detail.key} detail={detail} />
+              return <Detail key={detail.key} detail={detail}/>
             })
           }
         </div>
-      </CSSTransition>
+      </Transition>
     </section>
   )
 }
