ba2d29d684f3196f130ed1ff63d1c8ebe3467a88
tangchenming <tangchenming@hikvision.com>
2024-05-23 11:13:28 +0800

    style: Catalogue 组件数据传参修改
    Signed-off-by: tangchenming <tangchenming@hikvision.com>

--- src/modules/popup/index.tsx
+++ src/modules/popup/index.tsx
@@ -1,4 +1,4 @@
-import React, { StrictMode } from 'react'
+import React, { StrictMode, useState } from 'react'
 import ReactDom from 'react-dom/client'
 import { Divider, Catalogue, Button } from '@/components'
 import './index.scss'
@@ -6,16 +6,68 @@ import './index.scss'
 import ToyEditor from '@/components/toy-editor'
 
 const App: React.FC = () => {
+  const [catalogues, setCatalogues] = useState<ToyComponent.CatalogueItem[]>([
+    {
+      title: '工作日志',
+      key: '工作日志',
+      children: [
+        {
+          title: '2024年3月11日工作日志',
+          key: '0-0-0-0'
+        },
+        {
+          title: '2024年3月12日工作日志',
+          key: '0-0-0-1',
+        },
+        {
+          title: '2024年3月13日工作日志',
+          key: '0-0-0-2',
+        },
+        {
+          title: '2024年3月14日工作日志',
+          key: '0-0-0-3',
+        },
+        {
+          title: '2024年3月15日工作日志',
+          key: '0-0-0-4',
+        }
+      ]
+    },
+    {
+      title: '请假记录',
+      key: '请假记录',
+      children: [
+        {
+          title: '2024年4与12日请假记录',
+          key: '0-0-0-0',
+        }
+      ]
+    }
+  ])
+  const [artists, setArtists] = useState<any[]>([])
+
+  const addGather = () => {
+    const empty: ToyComponent.CatalogueItem = {
+      title: '1111',
+      key: '2222',
+      open: false,
+      isEdit: true,
+      children: []
+    }
+    setCatalogues([...catalogues, empty])
+    console.log(catalogues)
+  }
+
   return (
     <StrictMode>
       <div className="app">
         <div className="content">
           <section className="catalogue-wrapper">
             <div className="actions-group">
-              <Button size="mini" type="default" >增加文件夹</Button>
+              <Button size="mini" type="default" onClick={addGather}>增加文件夹</Button>
             </div>
             <Divider style={{ margin: '8px 0' }} direction="horizontal" />
-            <Catalogue />
+            <Catalogue data={catalogues} />
           </section>
           <Divider direction="vertical" style={{ margin: '0 6px' }} />
           <ToyEditor/>
